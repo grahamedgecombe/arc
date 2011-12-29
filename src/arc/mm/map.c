2cdb1c75494db6d6edb813f5eb1f9736f019936e
Graham Edgecombe <graham@grahamedgecombe.com>
2011-12-29 21:33:48 +0000

    Rewrote mm_map_sanitize(). The code is naive in places, but very simple to understand and covers the case of multiple regions overlapping another region.

--- src/arc/mm/map.c
+++ src/arc/mm/map.c
@@ -23,8 +23,11 @@
 /* virtual memory offset for transforming _start/_end to physical addresses */
 #define VM_OFFSET 0xFFFF800000000000
 
-static int mm_map_count = 0;
-static mm_map_entry_t mm_map_entries[MM_MAP_MAX_ENTRIES];
+/* unused type id (prefixed to fit in with other #defines from multiboot.h) */
+#define MULTIBOOT_MMAP_UNUSED 0
+
+static int entry_count = 0;
+static mm_map_entry_t entries[MM_MAP_MAX_ENTRIES];
 
 static const char *mm_map_type_desc(int type)
 {
@@ -41,196 +44,129 @@ static const char *mm_map_type_desc(int type)
 
 static void mm_map_add(int type, uintptr_t addr_start, uintptr_t addr_end)
 {
-  mm_map_entries[mm_map_count].type = type;
-  mm_map_entries[mm_map_count].addr_start = addr_start;
-  mm_map_entries[mm_map_count++].addr_end = addr_end;
+  for (int id = 0; id < entry_count; id++)
+  {
+    mm_map_entry_t *entry = &entries[id];
+    if (entry->type == MULTIBOOT_MMAP_UNUSED)
+    {
+      entry->type = type;
+      entry->addr_start = addr_start;
+      entry->addr_end = addr_end;
+      return;
+    }
+  }
+
+  if (entry_count == MM_MAP_MAX_ENTRIES)
+  {
+    /* TODO: the memory map is full - abort boot and display error message */
+  }
+
+  mm_map_entry_t *entry = &entries[entry_count++];
+  entry->type = type;
+  entry->addr_start = addr_start;
+  entry->addr_end = addr_end;
 }
 
-static void mm_map_remove(int id)
-{
-  int shift = mm_map_count - 1 - id;
-  memmove(&mm_map_entries[id], &mm_map_entries[id + 1], sizeof(mm_map_entries) * shift);
-  mm_map_count--;
-}
-
+/* a rather naive function that sanitizes the memory map */
 static void mm_map_sanitize(void)
 {
-  /* we don't need to do anything if there are zero or one entries */
-  if (mm_map_count <= 1)
-    return;
-
-  /* create a temporary, read-only copy of the original map */
-  int tmp_count = mm_map_count;
-  static mm_map_entry_t tmp_map[MM_MAP_MAX_ENTRIES];
-  memcpy(tmp_map, mm_map_entries, sizeof(mm_map_entries));
-
-  /* fix overlapping entries */
-  for (int id = 0; id < tmp_count; id++)
+  /* do as many passes as required to fully simplify the map */
+next_pass:
+  for (int id = 0; id < entry_count; id++)
   {
-    uintptr_t start = tmp_map[id].addr_start;
-    uintptr_t end = tmp_map[id].addr_end;
-    int type = tmp_map[id].type;
-
-    for (int inner_id = 0; inner_id < tmp_count; inner_id++)
+   for (int inner_id = 0; inner_id < entry_count; inner_id++)
     {
-      uintptr_t inner_start = tmp_map[inner_id].addr_start;
-      uintptr_t inner_end = tmp_map[inner_id].addr_end;
-      int inner_type = tmp_map[inner_id].type;
-
-      /*
-       * higher type ids override lower ids (implemented like this in Linux),
-       * also ignore ourself
-       */
-      if (inner_type <= type || id == inner_id)
+      mm_map_entry_t entry = entries[id];
+      if (entry.type == MULTIBOOT_MMAP_UNUSED)
         continue;
 
-      /*
-       * check for overlaps
-       *
-       * outer overlaps all of the inner condition:
-       *
-       *    s      e
-       *    v      v
-       *   -11111111-
-       *   ---2222---
-       *      ^  ^
-       *     is  ie
-       *
-       * outer overlaps inner end condition:
-       *       s  e
-       *       v  v
-       *   ----1111--
-       *   --2222----
-       *     ^  ^
-       *    is  ie
-       *
-       * outer overlaps inner start condition:
-       *
-       *     s  e
-       *     v  v
-       *   --1111----
-       *   ----2222--
-       *       ^  ^
-       *      is  ie
-       */
-      bool outer_overlaps_inner_end = inner_end >= start && inner_end <= end;
-      bool outer_overlaps_inner_start = inner_start >= start && inner_start <= end;
+      mm_map_entry_t inner_entry = entries[inner_id];
+      if (inner_entry.type == MULTIBOOT_MMAP_UNUSED)
+        continue;
 
-      /* fix the situation where both conditions are true */
-      if (outer_overlaps_inner_end && outer_overlaps_inner_start)
+      /* try to coalesce adjacent entries of the same type */
+      if (entry.type == inner_entry.type && (entry.addr_end + 1) == inner_entry.addr_start)
       {
-        /* mark the outer entry as a candidate for deletion */
-        mm_map_entries[id].type = 0;
-
-        /* create a new 'prefix' outer entry */
-        if (start != inner_start && start != (inner_start - 1))
-          mm_map_add(type, start, inner_start - 1);
-
-        /* create a new 'postfix' outer entry */
-        if (end != inner_end && end != (inner_end + 1))
-          mm_map_add(type, inner_end + 1, end);
+        entries[id].addr_end = inner_entry.addr_end;
+        entries[inner_id].type = MULTIBOOT_MMAP_UNUSED;
+        goto next_pass;
       }
-      /* fix the first kind of overlap */
-      else if (outer_overlaps_inner_end)
+
+      /* deal with overlapping entries - higher type ids take precedence */
+      if (inner_entry.type > entry.type)
       {
-        mm_map_entries[id].addr_start = mm_map_entries[inner_id].addr_end + 1;
-      }
-      /* fix the second kind of overlap */
-      else if (outer_overlaps_inner_start)
-      {
-        mm_map_entries[id].addr_end = mm_map_entries[inner_id].addr_start - 1;
+        /* inner encloses */
+        if (entry.addr_start >= inner_entry.addr_start && entry.addr_end <= inner_entry.addr_end)
+        {
+          entries[id].type = MULTIBOOT_MMAP_UNUSED;
+          goto next_pass;
+        }
+        /* inner enclosed */
+        else if (inner_entry.addr_start >= entry.addr_start && inner_entry.addr_end <= entry.addr_end)
+        {
+          if (entry.addr_start != inner_entry.addr_start)
+            mm_map_add(entry.type, entry.addr_start, inner_entry.addr_start - 1);
+
+          if (entry.addr_end != inner_entry.addr_end)
+            mm_map_add(entry.type, inner_entry.addr_end + 1, entry.addr_end);
+
+          entries[id].type = MULTIBOOT_MMAP_UNUSED;
+          goto next_pass;
+        }
+        /* inner overlaps left side check */
+        else if (entry.addr_start >= inner_entry.addr_start && entry.addr_start <= inner_entry.addr_end)
+        {
+          entries[id].addr_start = inner_entry.addr_end + 1;
+          goto next_pass;
+        }
+        /* inner overlaps right side check */
+        else if (entry.addr_end >= inner_entry.addr_start && entry.addr_end <= inner_entry.addr_end)
+        {
+          entries[id].addr_end = inner_entry.addr_start - 1;
+          goto next_pass;
+        }
       }
     }
   }
 
-  /*
-   * remove entries where the length is zero or if they have been marked as a
-   * candidate for deletion
-   */
-  for (int id = tmp_count; id > 0; id--)
+  /* delete unused entries by re-adding them all */
+  mm_map_entry_t tmp_entries[MM_MAP_MAX_ENTRIES];
+  memcpy(tmp_entries, entries, sizeof(entries));
+
+  int tmp_entry_count = entry_count;
+  entry_count = 0;
+
+  for (int id = 0; id < tmp_entry_count; id++)
   {
-    mm_map_entry_t *entry = &mm_map_entries[id - 1];
-    if (entry->type == 0 || entry->addr_start == entry->addr_end)
-      mm_map_remove(id - 1);
+    mm_map_entry_t *entry = &tmp_entries[id];
+    if (entry->type != MULTIBOOT_MMAP_UNUSED)
+      mm_map_add(entry->type, entry->addr_start, entry->addr_end);
   }
 
-  /* if there are one or less entries we don't need to coalesce */
-  if (mm_map_count <= 1)
+  /* bail out right now if a sort isn't required */
+  if (entry_count <= 1)
     return;
 
-  /* make another copy of the temporary map */
-  tmp_count = mm_map_count;
-  memcpy(tmp_map, mm_map_entries, sizeof(mm_map_entries));
-
-  /* coalesce adjacent entries */
-  for (int id = 0; id < tmp_count; id++)
-  {
-    uintptr_t end = tmp_map[id].addr_end;
-    int type = tmp_map[id].type;
-
-    for (int inner_id = 0; inner_id < tmp_count; inner_id++)
-    {
-      uintptr_t inner_start = tmp_map[inner_id].addr_start;
-      uintptr_t inner_end = tmp_map[inner_id].addr_end;
-      int inner_type = tmp_map[inner_id].type;
-
-      /* ignore entries not of the same type or the same entry */
-      if (type != inner_type || id == inner_id)
-        continue;
-
-      /* check for adjacency and merge adjacent entries */
-      if ((end + 1) == inner_start)
-      {
-        mm_map_entries[inner_id].type = 0;
-        mm_map_entries[id].addr_end = inner_end;
-      }
-    }
-  }
-
-  /* if there are one or less entries we don't need to coalesce */
-  if (mm_map_count <= 1)
-    return;
-
-  /*
-   * remove entries if they have been marked as a candidate for deletion
-   */
-  for (int id = tmp_count; id > 0; id--)
-  {
-    mm_map_entry_t *entry = &mm_map_entries[id - 1];
-    if (entry->type == 0)
-      mm_map_remove(id - 1);
-  }
-
-
-  /* if there are one or less entries we don't need to sort */
-  if (mm_map_count <= 1)
-    return;
-
-  /* sort all the entries */
+  /* sort the entries */
   for (;;)
   {
-    /* indicates if any swaps were made in this pass */
-    bool swapped = false;
+    /* a flag that indicates if any swaps were performed this pass */
+    bool swap = false;
 
-    /* loop through all pairs of entries */
-    for (int id = 0; id < mm_map_count - 1; id++)
+    /* iterate through the list swapping adjacent pairs if required */
+    for (int id = 0; id < entry_count - 1; id++)
     {
-      /* check if we need to swap this pair */
-      uintptr_t first = mm_map_entries[id].addr_start;
-      uintptr_t second = mm_map_entries[id + 1].addr_start;
-      if (first > second)
+      if (entries[id].addr_start > entries[id + 1].addr_start)
       {
-        /* actually swap them */
-        mm_map_entry_t tmp;
-        memcpy(&tmp, &mm_map_entries[id], sizeof(tmp));
-        memcpy(&mm_map_entries[id], &mm_map_entries[id + 1], sizeof(tmp));
-        memcpy(&mm_map_entries[id + 1], &tmp, sizeof(tmp));
-        swapped = true;
+        mm_map_entry_t tmp = entries[id];
+        entries[id] = entries[id + 1];
+        entries[id + 1] = tmp;
+        swap = true;
       }
     }
 
-    /* if no swaps were made we can break out of the loop right now */
-    if (!swapped)
+    /* stop when the list is sorted */
+    if (!swap)
       break;
   }
 }
@@ -245,12 +181,13 @@ void mm_map_init(multiboot_t *multiboot)
   while (mmap_addr < (multiboot->mmap_addr + multiboot->mmap_len))
   {
     multiboot_mmap_t *mmap = (multiboot_mmap_t *) aphy32_to_virt(mmap_addr);
-    mm_map_add(mmap->type, mmap->addr, mmap->addr + mmap->len - 1);
+    if (mmap->len > 0)
+      mm_map_add(mmap->type, mmap->addr, mmap->addr + mmap->len - 1);
     mmap_addr += mmap->size + sizeof(mmap->size);
   }
 
   /* reserve kernel memory */
-  extern int _start, _end; /* these are placed at the start and end of the kernel image */
+  extern int _start, _end;
   uintptr_t start_addr = (uintptr_t) &_start - VM_OFFSET;
   uintptr_t end_addr   = (uintptr_t) &_end   - VM_OFFSET - 1;
   mm_map_add(MULTIBOOT_MMAP_RESERVED, start_addr, end_addr);
@@ -259,12 +196,15 @@ void mm_map_init(multiboot_t *multiboot)
   mm_map_sanitize();
 
   /* print the final map */
-  for (int id = 0; id < mm_map_count; id++)
+  for (int id = 0; id < entry_count; id++)
   {
-    uintptr_t start = mm_map_entries[id].addr_start;
-    uintptr_t end = mm_map_entries[id].addr_end;
-    int type = mm_map_entries[id].type;
-    tty_printf(" => 0x%016x -> 0x%016x (%s)\n", start, end, mm_map_type_desc(type));
+    uintptr_t start = entries[id].addr_start;
+    uintptr_t end = entries[id].addr_end;
+    int type = entries[id].type;
+    if (type != MULTIBOOT_MMAP_UNUSED)
+      tty_printf(" => 0x%016x -> 0x%016x (%s)\n", start, end, mm_map_type_desc(type));
   }
+
+  for (;;); /*busyloop so qemu refreshes the screen*/
 }
 
