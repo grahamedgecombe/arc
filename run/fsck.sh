63329519a98bc161cc5aca40ddf414b1481a0722
Graham Edgecombe <graham@grahamedgecombe.com>
2012-01-06 17:19:16 +0000

    Added a script for running a file system check on the disk image.

---
+++ run/fsck.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+BASEDIR=`dirname $0`
+cd $BASEDIR
+sudo losetup /dev/loop0 disk.img
+sudo losetup -o 32256 /dev/loop1 /dev/loop0
+sudo e2fsck -f /dev/loop1
+sudo losetup -d /dev/loop1
+sudo losetup -d /dev/loop0
+
