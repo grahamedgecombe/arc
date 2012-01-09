c42a115bf180a636fe1838f7950fde37bff3a3f7
Graham Edgecombe <graham@grahamedgecombe.com>
2012-01-09 12:26:27 +0000

    Added (untested) ioapic_route() function.

--- src/arc/intr/ioapic.c
+++ src/arc/intr/ioapic.c
@@ -15,7 +15,9 @@
  */
 
 #include <arc/intr/ioapic.h>
+#include <arc/intr/common.h>
 #include <arc/mm/phy32.h>
+#include <stdbool.h>
 
 static uint32_t ioapic_read(uint32_t mmio_addr, uint32_t reg)
 {
@@ -36,5 +38,24 @@ static void ioapic_write(uint32_t mmio_addr, uint32_t reg, uint32_t val)
 void ioapic_init(uint32_t mmio_addr)
 {
 
+}
+
+void ioapic_route(uint32_t mmio_addr, intr_id_t src, intr_id_t vec, bool high, bool lt)
+{
+  uint8_t dst = 0xFF;
+  uint64_t redtbl_entry = REDTBL_DESTMOD_PHYSICAL | REDTBL_DELMOD_FIXED | (((uint64_t) (dst & 0xFF)) << 56) | (vec & 0xFF);
+
+  if (high)
+    redtbl_entry |= REDTBL_ACTIVE_HIGH;
+  else
+    redtbl_entry |= REDTBL_ACTIVE_LOW;
+
+  if (lt)
+    redtbl_entry |= REDTBL_TRIGGER_LEVEL;
+  else
+    redtbl_entry |= REDTBL_TRIGGER_EDGE;
+
+  ioapic_write(mmio_addr, IOAPIC_REDTBL0 + 2 * src, redtbl_entry & 0xFFFFFFFF);
+  ioapic_write(mmio_addr, IOAPIC_REDTBL0 + 2 * src + 1, (redtbl_entry >> 32) & 0xFFFFFFFF);
 }
 
