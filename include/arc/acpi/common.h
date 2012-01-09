ae4dfae0f97a66dcea51dd3acb2efc05e11351e2
Graham Edgecombe <graham@grahamedgecombe.com>
2012-01-09 12:26:40 +0000

    Added ACPI RSDP, RSDT and XSDT structures.

---
+++ include/arc/acpi/common.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2011-2012 Graham Edgecombe <graham@grahamedgecombe.com>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef ARC_ACPI_COMMON_H
+#define ARC_ACPI_COMMON_H
+
+#include <stdint.h>
+#include <arc/pack.h>
+
+#define OEM_ID_LEN 6
+
+typedef PACK(struct
+{
+  uint32_t signature;
+  uint32_t len;
+  uint8_t  revision;
+  uint8_t  checksum;
+  char     oem_id[OEM_ID_LEN];
+  uint64_t oem_table_id;
+  uint32_t oem_revision;
+  uint32_t creator_id;
+  uint32_t creator_revision;
+}) acpi_header_t;
+
+#endif
+
---
+++ include/arc/acpi/rsdp.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2011-2012 Graham Edgecombe <graham@grahamedgecombe.com>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef ARC_ACPI_RSDP_H
+#define ARC_ACPI_RSDP_H
+
+#include <stdint.h>
+#include <arc/pack.h>
+#include <arc/acpi/common.h>
+
+#define RSDP_SIGNATURE 0x2052545020445352 /* 'RSD PTR ' */
+
+typedef PACK(struct
+{
+  /* original rsdp structure */
+  uint64_t signature;
+  uint8_t  checksum;
+  char     oem_id[OEM_ID_LEN];
+  uint8_t  revision;
+  uint32_t rsdt_addr;
+
+  /* extended fields - present if revision >= 2 */
+  uint32_t len;
+  uint64_t xsdt_addr;
+  uint8_t  ext_checksum;
+  uint8_t  reserved[3];
+}) rsdp_t;
+
+rsdp_t *rsdp_scan(void);
+
+#endif
+
