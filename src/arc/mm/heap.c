253325bbdfcdbe1b40df614045de872c9276e0d9
Graham Edgecombe <graham@grahamedgecombe.com>
2012-01-11 13:20:27 +0000

    Added code to initialise the root heap node.

--- src/arc/mm/heap.c
+++ src/arc/mm/heap.c
@@ -16,8 +16,26 @@
 
 #include <arc/mm/heap.h>
 #include <arc/mm/pmm.h>
+#include <arc/mm/vmm.h>
 #include <arc/mm/align.h>
+#include <arc/pack.h>
+#include <arc/panic.h>
 #include <stdint.h>
+
+/* the states a heap node can be in */
+#define HEAP_NODE_FREE      0 /* not allocated */
+#define HEAP_NODE_RESERVED  1 /* allocated, physical frames not managed by us */
+#define HEAP_NODE_ALLOCATED 2 /* allocated, physical frames managed by us */
+
+typedef PACK(struct heap_node
+{
+  struct heap_node *next;
+  int state;
+  uintptr_t start; /* the address of the first page, inclusive */
+  uintptr_t end;   /* the address of the last page, exclusive */
+}) heap_node_t;
+
+static heap_node_t *root_node;
 
 void heap_init(void)
 {
@@ -27,5 +45,20 @@ void heap_init(void)
 
   /* hard coded end of the heap (exclusive) */
   uintptr_t heap_end = VM_STACK_OFFSET;
+
+  /* allocate some space for the root node */
+  void *root_phy = pmm_alloc();
+  if (!root_phy)
+    boot_panic("couldn't allocate physical frame for heap root node");
+
+  /* the root node will take the first virtual address */
+  root_node = (heap_node_t *) heap_start;
+  vmm_map(heap_start, (uintptr_t) root_phy, PG_WRITABLE | PG_NO_EXEC);
+
+  /* fill out the root node */
+  root_node->next = 0;
+  root_node->state = HEAP_NODE_FREE;
+  root_node->start = heap_start + FRAME_SIZE;
+  root_node->end = heap_end;
 }
 
