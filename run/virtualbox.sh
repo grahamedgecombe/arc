ebe0a207bd9dcc6f9e222ac92c728e734a6993ac
Graham Edgecombe <graham@grahamedgecombe.com>
2012-01-12 20:05:41 +0000

    Enable ACPI in VirtualBox.

--- run/virtualbox.sh
+++ run/virtualbox.sh
@@ -8,7 +8,7 @@ if [ -f disk.vdi ]; then
 fi
 VBoxManage convertfromraw disk.img disk.vdi
 VBoxManage createvm --name arc --register
-VBoxManage modifyvm arc --acpi off --ioapic on --cpus 2 --memory 128 --boot1 disk --boot2 none --boot3 none --boot4 none
+VBoxManage modifyvm arc --acpi on --ioapic on --cpus 2 --memory 128 --boot1 disk --boot2 none --boot3 none --boot4 none
 VBoxManage storagectl arc --name "SATA Controller" --add sata
 VBoxManage storageattach arc --storagectl "SATA Controller" --port 0 --type hdd --medium disk.vdi
 VBoxManage startvm arc
