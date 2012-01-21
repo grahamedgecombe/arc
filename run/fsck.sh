90a80de40f29f44f0fb4f6b3c8df9a84da4ad829
Graham Edgecombe <graham@grahamedgecombe.com>
2012-01-21 13:06:26 +0000

    Improved the fsck script further to stop problems if two scripts are competing access to the loop devices simultaneously.

--- run/fsck.sh
+++ run/fsck.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 BASEDIR=`dirname $0`
 cd $BASEDIR
-LOOP0=`sudo losetup -f`
-sudo losetup $LOOP0 disk.img
-LOOP1=`sudo losetup -f`
-sudo losetup -o 32256 $LOOP1 $LOOP0
+LOOP0=`sudo losetup -f --show disk.img`
+LOOP1=`sudo losetup -o 32256 -f --show $LOOP0`
 sudo e2fsck -f $LOOP1
 sync
 sudo losetup -d $LOOP1
