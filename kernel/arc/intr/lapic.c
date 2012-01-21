d41506cddee05d4557eb65ce56831b84d75c87ec
Graham Edgecombe <graham@grahamedgecombe.com>
2012-01-21 12:29:12 +0000

    Refactored SPURIOUS_VEC -> SVR.

--- kernel/arc/intr/lapic.c
+++ kernel/arc/intr/lapic.c
@@ -20,53 +20,53 @@
 #include <arc/mm/mmio.h>
 #include <arc/intr/common.h>
 
-#define LAPIC_ID           0x08
-#define LAPIC_VER          0x0C
-#define LAPIC_TPR          0x20
-#define LAPIC_APR          0x24
-#define LAPIC_PPR          0x28
-#define LAPIC_EOI          0x2C
-#define LAPIC_RRD          0x30
-#define LAPIC_SPURIOUS_VEC 0x3C
-#define LAPIC_LDR          0x34
-#define LAPIC_DFR          0x38
-#define LAPIC_ISR0         0x40
-#define LAPIC_ISR1         0x44
-#define LAPIC_ISR2         0x48
-#define LAPIC_ISR3         0x4C
-#define LAPIC_ISR4         0x50
-#define LAPIC_ISR5         0x54
-#define LAPIC_ISR6         0x58
-#define LAPIC_ISR7         0x5C
-#define LAPIC_TMR0         0x60
-#define LAPIC_TMR1         0x64
-#define LAPIC_TMR2         0x68
-#define LAPIC_TMR3         0x6C
-#define LAPIC_TMR4         0x70
-#define LAPIC_TMR5         0x74
-#define LAPIC_TMR6         0x78
-#define LAPIC_TMR7         0x7C
-#define LAPIC_IRR0         0x80
-#define LAPIC_IRR1         0x84
-#define LAPIC_IRR2         0x88
-#define LAPIC_IRR3         0x8C
-#define LAPIC_IRR4         0x90
-#define LAPIC_IRR5         0x94
-#define LAPIC_IRR6         0x98
-#define LAPIC_IRR7         0x9C
-#define LAPIC_ESR          0xA0
-#define LAPIC_LVT_CMCI     0xBC
-#define LAPIC_ICR0         0xC0
-#define LAPIC_ICR1         0xC4
-#define LAPIC_LVT_TIMER    0xC8
-#define LAPIC_LVT_LINT0    0xD4
-#define LAPIC_LVT_LINT1    0xD8
-#define LAPIC_LVT_ERROR    0xDC
-#define LAPIC_TIMER_ICR    0xE0
-#define LAPIC_TIMER_CCR    0xE4
-#define LAPIC_TIMER_DCR    0xF8
+#define LAPIC_ID        0x08
+#define LAPIC_VER       0x0C
+#define LAPIC_TPR       0x20
+#define LAPIC_APR       0x24
+#define LAPIC_PPR       0x28
+#define LAPIC_EOI       0x2C
+#define LAPIC_RRD       0x30
+#define LAPIC_SVR       0x3C
+#define LAPIC_LDR       0x34
+#define LAPIC_DFR       0x38
+#define LAPIC_ISR0      0x40
+#define LAPIC_ISR1      0x44
+#define LAPIC_ISR2      0x48
+#define LAPIC_ISR3      0x4C
+#define LAPIC_ISR4      0x50
+#define LAPIC_ISR5      0x54
+#define LAPIC_ISR6      0x58
+#define LAPIC_ISR7      0x5C
+#define LAPIC_TMR0      0x60
+#define LAPIC_TMR1      0x64
+#define LAPIC_TMR2      0x68
+#define LAPIC_TMR3      0x6C
+#define LAPIC_TMR4      0x70
+#define LAPIC_TMR5      0x74
+#define LAPIC_TMR6      0x78
+#define LAPIC_TMR7      0x7C
+#define LAPIC_IRR0      0x80
+#define LAPIC_IRR1      0x84
+#define LAPIC_IRR2      0x88
+#define LAPIC_IRR3      0x8C
+#define LAPIC_IRR4      0x90
+#define LAPIC_IRR5      0x94
+#define LAPIC_IRR6      0x98
+#define LAPIC_IRR7      0x9C
+#define LAPIC_ESR       0xA0
+#define LAPIC_LVT_CMCI  0xBC
+#define LAPIC_ICR0      0xC0
+#define LAPIC_ICR1      0xC4
+#define LAPIC_LVT_TIMER 0xC8
+#define LAPIC_LVT_LINT0 0xD4
+#define LAPIC_LVT_LINT1 0xD8
+#define LAPIC_LVT_ERROR 0xDC
+#define LAPIC_TIMER_ICR 0xE0
+#define LAPIC_TIMER_CCR 0xE4
+#define LAPIC_TIMER_DCR 0xF8
 
-#define SPURIOUS_VEC_ENABLED 0x100
+#define SVR_ENABLED 0x100
 
 static volatile uint32_t *lapic;
 static uintptr_t lapic_phy_addr;
@@ -96,7 +96,7 @@ void lapic_init(void)
   uint64_t apic_base = (msr_read(MSR_APIC_BASE) & APIC_BASE_BSP) | lapic_phy_addr | APIC_BASE_ENABLE;
   msr_write(MSR_APIC_BASE, apic_base);
 
-  lapic_write(LAPIC_SPURIOUS_VEC, SPURIOUS_VEC_ENABLED | SPURIOUS);
+  lapic_write(LAPIC_SVR, SVR_ENABLED | SPURIOUS);
 }
 
 void lapic_ack(void)
--- kernel/arc/intr/lx2apic.c
+++ kernel/arc/intr/lx2apic.c
@@ -18,50 +18,50 @@
 #include <arc/cpu/msr.h>
 #include <arc/intr/common.h>
 
-#define LX2APIC_ID           0x802
-#define LX2APIC_VER          0x803
-#define LX2APIC_TPR          0x808
-#define LX2APIC_PPR          0x80A
-#define LX2APIC_EOI          0x80B
-#define LX2APIC_SPURIOUS_VEC 0x80F
-#define LX2APIC_LDR          0x80D
-#define LX2APIC_ISR0         0x810
-#define LX2APIC_ISR1         0x811
-#define LX2APIC_ISR2         0x812
-#define LX2APIC_ISR3         0x813
-#define LX2APIC_ISR4         0x814
-#define LX2APIC_ISR5         0x815
-#define LX2APIC_ISR6         0x816
-#define LX2APIC_ISR7         0x817
-#define LX2APIC_TMR0         0x818
-#define LX2APIC_TMR1         0x819
-#define LX2APIC_TMR2         0x81A
-#define LX2APIC_TMR3         0x81B
-#define LX2APIC_TMR4         0x81C
-#define LX2APIC_TMR5         0x81D
-#define LX2APIC_TMR6         0x81E
-#define LX2APIC_TMR7         0x81F
-#define LX2APIC_IRR0         0x820
-#define LX2APIC_IRR1         0x821
-#define LX2APIC_IRR2         0x822
-#define LX2APIC_IRR3         0x823
-#define LX2APIC_IRR4         0x824
-#define LX2APIC_IRR5         0x825
-#define LX2APIC_IRR6         0x826
-#define LX2APIC_IRR7         0x827
-#define LX2APIC_ESR          0x828
-#define LX2APIC_LVT_CMCI     0x82F
-#define LX2APIC_ICR          0x830
-#define LX2APIC_LVT_TIMER    0x832
-#define LX2APIC_LVT_LINT0    0x835
-#define LX2APIC_LVT_LINT1    0x836
-#define LX2APIC_LVT_ERROR    0x837
-#define LX2APIC_TIMER_ICR    0x838
-#define LX2APIC_TIMER_CCR    0x839
-#define LX2APIC_TIMER_DCR    0x83E
-#define LX2APIC_SELF_IPI     0x83F
+#define LX2APIC_ID        0x802
+#define LX2APIC_VER       0x803
+#define LX2APIC_TPR       0x808
+#define LX2APIC_PPR       0x80A
+#define LX2APIC_EOI       0x80B
+#define LX2APIC_SVR       0x80F
+#define LX2APIC_LDR       0x80D
+#define LX2APIC_ISR0      0x810
+#define LX2APIC_ISR1      0x811
+#define LX2APIC_ISR2      0x812
+#define LX2APIC_ISR3      0x813
+#define LX2APIC_ISR4      0x814
+#define LX2APIC_ISR5      0x815
+#define LX2APIC_ISR6      0x816
+#define LX2APIC_ISR7      0x817
+#define LX2APIC_TMR0      0x818
+#define LX2APIC_TMR1      0x819
+#define LX2APIC_TMR2      0x81A
+#define LX2APIC_TMR3      0x81B
+#define LX2APIC_TMR4      0x81C
+#define LX2APIC_TMR5      0x81D
+#define LX2APIC_TMR6      0x81E
+#define LX2APIC_TMR7      0x81F
+#define LX2APIC_IRR0      0x820
+#define LX2APIC_IRR1      0x821
+#define LX2APIC_IRR2      0x822
+#define LX2APIC_IRR3      0x823
+#define LX2APIC_IRR4      0x824
+#define LX2APIC_IRR5      0x825
+#define LX2APIC_IRR6      0x826
+#define LX2APIC_IRR7      0x827
+#define LX2APIC_ESR       0x828
+#define LX2APIC_LVT_CMCI  0x82F
+#define LX2APIC_ICR       0x830
+#define LX2APIC_LVT_TIMER 0x832
+#define LX2APIC_LVT_LINT0 0x835
+#define LX2APIC_LVT_LINT1 0x836
+#define LX2APIC_LVT_ERROR 0x837
+#define LX2APIC_TIMER_ICR 0x838
+#define LX2APIC_TIMER_CCR 0x839
+#define LX2APIC_TIMER_DCR 0x83E
+#define LX2APIC_SELF_IPI  0x83F
 
-#define SPURIOUS_VEC_ENABLED 0x100
+#define SVR_ENABLED 0x100
 
 static uint64_t lx2apic_read(uint32_t reg)
 {
@@ -78,7 +78,7 @@ void lx2apic_init(void)
   uint64_t apic_base = (msr_read(MSR_APIC_BASE) & APIC_BASE_BSP) | APIC_BASE_ENABLE | APIC_BASE_X2_MODE;
   msr_write(MSR_APIC_BASE, apic_base);
 
-  lx2apic_write(LX2APIC_SPURIOUS_VEC, SPURIOUS_VEC_ENABLED | SPURIOUS);
+  lx2apic_write(LX2APIC_SVR, SVR_ENABLED | SPURIOUS);
 }
 
 void lx2apic_ack(void)
