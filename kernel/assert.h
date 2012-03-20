8292cc7581b039f50666cfbd417dbfc76c0f6577
Graham Edgecombe <graham@grahamedgecombe.com>
2012-03-20 16:42:12 +0000

    Added simple assert.h implementation that calls panic().

---
+++ kernel/assert.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2011-2012 Graham Edgecombe <graham@grahamedgecombe.com>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef ASSERT_H
+#define ASSERT_H
+
+#include <arc/panic.h>
+
+#define assert(expr) ((expr) ? ((void) 0) : panic("%s:%d: %s: Assertion `%s' failed.", __FILE__, __LINE__, __func__, #expr))
+
+#endif
+
