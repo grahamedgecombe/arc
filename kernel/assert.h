4d452a342df03124b7d5ac0d3d1c7ef13d53b704
Graham Edgecombe <graham@grahamedgecombe.com>
2012-03-20 16:45:55 +0000

    Assertions are now ignored if NDEBUG is set.

--- kernel/assert.h
+++ kernel/assert.h
@@ -19,7 +19,11 @@
 
 #include <arc/panic.h>
 
+#ifdef NDEBUG
+#define assert(expr) ((void) 0)
+#else
 #define assert(expr) ((expr) ? ((void) 0) : panic("%s:%d: %s: Assertion `%s' failed.", __FILE__, __LINE__, __func__, #expr))
+#endif
 
 #endif
 
