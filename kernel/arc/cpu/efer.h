ad87403f0e81fd512974ace01d00724c60336674
Graham Edgecombe <graham@grahamedgecombe.com>
2012-05-05 22:37:55 +0100

    Fixed EFER_SCE flag.

--- kernel/arc/cpu/efer.h
+++ kernel/arc/cpu/efer.h
@@ -19,7 +19,7 @@
 
 #include <stdint.h>
 
-#define EFER_SCE 0x00000000 /* syscall enable */
+#define EFER_SCE 0x00000001 /* syscall enable */
 #define EFER_LME 0x00000100 /* long mode enable */
 #define EFER_LMA 0x00000400 /* long mode active */
 #define EFER_NXE 0x00000800 /* nx bit enable */
