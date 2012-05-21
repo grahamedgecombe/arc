62e374146fcd2069e8c697950f35fc32e640eba0
Graham Edgecombe <graham@grahamedgecombe.com>
2012-05-21 22:36:18 +0100

    Removed blank comment line and added static_assert.

--- kernel/arc/intr/apic.c
+++ kernel/arc/intr/apic.c
@@ -1,5 +1,4 @@
 /*
- * `
  * Copyright (c) 2011-2012 Graham Edgecombe <graham@grahamedgecombe.com>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
--- kernel/assert.h
+++ kernel/assert.h
@@ -16,6 +16,8 @@
 
 #include <arc/panic.h>
 
+#define static_assert _Static_assert
+
 #ifdef NDEBUG
 #define assert(expr) ((void) 0)
 #else
