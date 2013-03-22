ba3740bb2259519043d831a4c14bedb8b47437ea
Graham Edgecombe <graham@grahamedgecombe.com>
2013-03-22 15:48:08 +0000

    Add workaround to fix memset() in gcc 4.8 at -O3.

--- kernel/string/memset.c
+++ kernel/string/memset.c
@@ -18,7 +18,9 @@
 
 void *memset(void *ptr, int value, size_t len)
 {
-  char *ptr8 = (char *) ptr;
+  // TODO: volatile is a hacky workaround, gcc 4.8.0 at -O3 seems to optimize
+  // away most of the code here...
+  volatile char *ptr8 = (volatile char *) ptr;
 
   while (len--)
     *ptr8++ = value;
