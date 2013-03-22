916afa2785cedefe7bb0c04ac133246bccf6d986
Graham Edgecombe <graham@grahamedgecombe.com>
2013-03-22 16:03:01 +0000

    Use new offset in run/fsck.sh.

--- run/fsck.sh
+++ run/fsck.sh
@@ -2,7 +2,7 @@
 BASEDIR=`dirname $0`
 cd $BASEDIR
 LOOP0=`sudo losetup -f --show disk.img`
-LOOP1=`sudo losetup -o 32256 -f --show $LOOP0`
+LOOP1=`sudo losetup -o 1048576 -f --show $LOOP0`
 sudo e2fsck -f $LOOP1
 sync
 sudo losetup -d $LOOP1
