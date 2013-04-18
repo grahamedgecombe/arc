156aa5902b66643d7c2b4fbddfb48d6f9473c1b5
Graham Edgecombe <graham@grahamedgecombe.com>
2013-04-18 20:46:50 +0100

    Add memcmp().

--- kernel/string/memcmp.c
+++ kernel/string/memcmp.c
@@ -14,17 +14,22 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#ifndef STRING_H
-#define STRING_H
+#include <string.h>
 
-#include <stddef.h>
+int memcmp(const void *ptr1, const void *ptr2, size_t len)
+{
+  const unsigned char *p1 = (const unsigned char *) ptr1;
+  const unsigned char *p2 = (const unsigned char *) ptr2;
 
-int strcmp(const char *str1, const char *str2);
-size_t strlen(const char *str);
-void *memclr(void *ptr, size_t len);
-void *memset(void *ptr, int value, size_t len);
-void *memcpy(void *dst, const void *src, size_t len);
-void *memmove(void *dst, const void *src, size_t len);
-int memcmp(const void *ptr1, const void *ptr2, size_t len);
+  while (len--)
+  {
+    if (*p1 != *p2)
+      return *p1 - *p2;
 
-#endif
+    p1++;
+    p2++;
+  }
+
+  return 0;
+
+}
