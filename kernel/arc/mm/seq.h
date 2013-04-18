3bb1c5808caf0e7f0f1d44f1c0be6cf6adc49365
Graham Edgecombe <graham@grahamedgecombe.com>
2013-04-18 20:40:18 +0100

    Add sequential allocator (without free support) for use during early boot.

---
+++ kernel/arc/mm/seq.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2011-2013 Graham Edgecombe <graham@grahamedgecombe.com>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef ARC_MM_SEQ_H
+#define ARC_MM_SEQ_H
+
+#include <stddef.h>
+
+/*
+ * Allocates memory from a small 'pool' that can be used before the real
+ * allocator has been set up. This memory can never be freed, and the pool is
+ * small, so this should be used only when absolutely necessary.
+ */
+void *seq_alloc(size_t len);
+
+void seq_trace(void);
+
+#endif
