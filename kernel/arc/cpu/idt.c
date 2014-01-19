02050a330d67391b299ab25a160ded44b4d1b163
Graham Edgecombe <graham@grahamedgecombe.com>
2014-01-19 11:32:26 +0000

    Update idt.c naming to matching gdt.c.

--- kernel/arc/cpu/idt.c
+++ kernel/arc/cpu/idt.c
@@ -20,10 +20,10 @@
 #include <arc/types.h>
 #include <string.h>
 
-#define GATE_PRESENT   0x80
-#define GATE_USER      0x60
-#define GATE_INTERRUPT 0x0E
-#define GATE_TRAP      0x0F
+#define IDT_PRESENT   0x80
+#define IDT_USER      0x60
+#define IDT_INTERRUPT 0x0E
+#define IDT_TRAP      0x0F
 
 typedef struct
 {
@@ -34,93 +34,93 @@ typedef struct
   uint16_t addr_mid;
   uint32_t addr_high;
   uint32_t reserved2;
-} __attribute__((__packed__)) idt_gate_t;
+} __attribute__((__packed__)) idt_descriptor_t;
 
-static idt_gate_t idt_gates[INTERRUPTS];
+static idt_descriptor_t idt_descriptors[INTERRUPTS];
 static idtr_t idtr;
 
-static void idt_encode_gate(intr_t id, void (*handler)(void), uint8_t flags)
+static void idt_encode_descriptor(intr_t id, void (*handler)(void), uint8_t flags)
 {
-  idt_gate_t *gate = &idt_gates[id];
+  idt_descriptor_t *descriptor = &idt_descriptors[id];
 
-  gate->cs_sel = 0x08;
+  descriptor->cs_sel = 0x08;
 
   uintptr_t handler_addr = (uintptr_t) handler;
-  gate->addr_low = handler_addr & 0xFFFF;
-  gate->addr_mid = (handler_addr >> 16) & 0xFFFF;
-  gate->addr_high = (handler_addr >> 32) & 0xFFFFFFFF;
+  descriptor->addr_low = handler_addr & 0xFFFF;
+  descriptor->addr_mid = (handler_addr >> 16) & 0xFFFF;
+  descriptor->addr_high = (handler_addr >> 32) & 0xFFFFFFFF;
 
-  gate->flags = flags;
+  descriptor->flags = flags;
 }
 
 void idt_bsp_init(void)
 {
-  memset(idt_gates, 0, sizeof(idt_gates));
+  memset(idt_descriptors, 0, sizeof(idt_descriptors));
 
-  idt_encode_gate(FAULT0,    &fault0,    GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(FAULT1,    &fault1,    GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(FAULT2,    &fault2,    GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(FAULT3,    &fault3,    GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(FAULT4,    &fault4,    GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(FAULT5,    &fault5,    GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(FAULT6,    &fault6,    GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(FAULT7,    &fault7,    GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(FAULT8,    &fault8,    GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(FAULT9,    &fault9,    GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(FAULT10,   &fault10,   GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(FAULT11,   &fault11,   GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(FAULT12,   &fault12,   GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(FAULT13,   &fault13,   GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(FAULT14,   &fault14,   GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(FAULT15,   &fault15,   GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(FAULT16,   &fault16,   GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(FAULT17,   &fault17,   GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(FAULT18,   &fault18,   GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(FAULT19,   &fault19,   GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(FAULT20,   &fault20,   GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(FAULT21,   &fault21,   GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(FAULT22,   &fault22,   GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(FAULT23,   &fault23,   GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(FAULT24,   &fault24,   GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(FAULT25,   &fault25,   GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(FAULT26,   &fault26,   GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(FAULT27,   &fault27,   GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(FAULT28,   &fault28,   GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(FAULT29,   &fault29,   GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(FAULT30,   &fault30,   GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(FAULT31,   &fault31,   GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(IRQ0,      &irq0,      GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(IRQ1,      &irq1,      GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(IRQ2,      &irq2,      GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(IRQ3,      &irq3,      GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(IRQ4,      &irq4,      GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(IRQ5,      &irq5,      GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(IRQ6,      &irq6,      GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(IRQ7,      &irq7,      GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(IRQ8,      &irq8,      GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(IRQ9,      &irq9,      GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(IRQ10,     &irq10,     GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(IRQ11,     &irq11,     GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(IRQ12,     &irq12,     GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(IRQ13,     &irq13,     GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(IRQ14,     &irq14,     GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(IRQ15,     &irq15,     GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(IRQ16,     &irq16,     GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(IRQ17,     &irq17,     GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(IRQ18,     &irq18,     GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(IRQ19,     &irq19,     GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(IRQ20,     &irq20,     GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(IRQ21,     &irq21,     GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(IRQ22,     &irq22,     GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(IRQ23,     &irq23,     GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(IPI_PANIC, &ipi_panic, GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(IPI_TLB,   &ipi_tlb,   GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(LVT_TIMER, &lvt_timer, GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(LVT_ERROR, &lvt_error, GATE_PRESENT | GATE_INTERRUPT);
-  idt_encode_gate(SPURIOUS,  &spurious,  GATE_PRESENT | GATE_INTERRUPT);
+  idt_encode_descriptor(FAULT0,    &fault0,    IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(FAULT1,    &fault1,    IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(FAULT2,    &fault2,    IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(FAULT3,    &fault3,    IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(FAULT4,    &fault4,    IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(FAULT5,    &fault5,    IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(FAULT6,    &fault6,    IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(FAULT7,    &fault7,    IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(FAULT8,    &fault8,    IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(FAULT9,    &fault9,    IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(FAULT10,   &fault10,   IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(FAULT11,   &fault11,   IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(FAULT12,   &fault12,   IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(FAULT13,   &fault13,   IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(FAULT14,   &fault14,   IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(FAULT15,   &fault15,   IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(FAULT16,   &fault16,   IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(FAULT17,   &fault17,   IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(FAULT18,   &fault18,   IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(FAULT19,   &fault19,   IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(FAULT20,   &fault20,   IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(FAULT21,   &fault21,   IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(FAULT22,   &fault22,   IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(FAULT23,   &fault23,   IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(FAULT24,   &fault24,   IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(FAULT25,   &fault25,   IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(FAULT26,   &fault26,   IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(FAULT27,   &fault27,   IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(FAULT28,   &fault28,   IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(FAULT29,   &fault29,   IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(FAULT30,   &fault30,   IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(FAULT31,   &fault31,   IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(IRQ0,      &irq0,      IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(IRQ1,      &irq1,      IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(IRQ2,      &irq2,      IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(IRQ3,      &irq3,      IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(IRQ4,      &irq4,      IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(IRQ5,      &irq5,      IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(IRQ6,      &irq6,      IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(IRQ7,      &irq7,      IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(IRQ8,      &irq8,      IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(IRQ9,      &irq9,      IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(IRQ10,     &irq10,     IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(IRQ11,     &irq11,     IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(IRQ12,     &irq12,     IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(IRQ13,     &irq13,     IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(IRQ14,     &irq14,     IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(IRQ15,     &irq15,     IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(IRQ16,     &irq16,     IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(IRQ17,     &irq17,     IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(IRQ18,     &irq18,     IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(IRQ19,     &irq19,     IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(IRQ20,     &irq20,     IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(IRQ21,     &irq21,     IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(IRQ22,     &irq22,     IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(IRQ23,     &irq23,     IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(IPI_PANIC, &ipi_panic, IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(IPI_TLB,   &ipi_tlb,   IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(LVT_TIMER, &lvt_timer, IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(LVT_ERROR, &lvt_error, IDT_PRESENT | IDT_INTERRUPT);
+  idt_encode_descriptor(SPURIOUS,  &spurious,  IDT_PRESENT | IDT_INTERRUPT);
 
-  idtr.addr = (uint64_t) idt_gates;
-  idtr.len = sizeof(idt_gates) - 1;
+  idtr.addr = (uint64_t) idt_descriptors;
+  idtr.len = sizeof(idt_descriptors) - 1;
   idtr_install(&idtr);
 }
 
