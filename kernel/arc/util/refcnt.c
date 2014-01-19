2989d8a799947e4d04f3f86abe24830045f9462a
Graham Edgecombe <graham@grahamedgecombe.com>
2014-01-19 20:02:45 +0000

    Add initial reference counting code.

---
+++ kernel/arc/util/refcnt.c
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2011-2014 Graham Edgecombe <graham@grahamedgecombe.com>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <arc/util/refcnt.h>
+#include <assert.h>
+
+void refcnt_init(refcnt_t *refcnt)
+{
+  refcnt->count = 1;
+}
+
+void refcnt_retain(refcnt_t *refcnt)
+{
+  // TODO maybe have some sanity checking for reaching UINT_MAX?
+  refcnt->count++;
+}
+
+void refcnt_release(refcnt_t *refcnt)
+{
+  assert(refcnt->count != 0);
+  refcnt->count--;
+}
